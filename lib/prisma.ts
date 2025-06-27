d7aa79c005503ec95ae1d1ef340967883b787329
Iqbal Ramadan <iqbalramadan3130@gmail.com>
2025-06-27 13:05:06 +0700

    chore: remove @prisma/extension-accelerate dependency and update prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '~/generated/client/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient()
 
 export default prisma
